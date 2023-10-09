bf684650c101c30d1a29819cc89201de8ad5f655
Saray Magallanes <smagarodri@gmail.com>
2023-10-09 13:49:57 +0200

    bombilla part1

---
+++ Bombilla/inputFilesTS/main.ts
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#bon")!;
+const options = document.querySelector("#options");
+
+btn.addEventListener("click", () => {
+    alert(1);
+}); 
+
---
+++ Bombilla/outputFilesJS/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const btn = document.querySelector("#bon");
+const options = document.querySelector("#options");
+btn.addEventListener("click", () => {
+    alert(1);
+});

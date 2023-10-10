320c8ad5615fc454e66d9c58274fbcba4ea1a59a
Saray Magallanes <smagarodri@gmail.com>
2023-10-10 13:57:04 +0200

    tamagochi part1

---
+++ Tamagochi/inputFilesTS/main.ts
@@ -0,0 +1,10 @@
+var circleBar = new ProgressBar.Circle('#circle-container', {
+    color: 'white',
+    strokeWidth: 2,
+    trailWidth: 10,
+    trailColor: 'black',
+    easing: 'easeInOut'
+});
+circleBar.animate(0.75, {
+    duration: 1500
+});
